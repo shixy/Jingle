1bee8fedd897459653a438a96f3cc25cabdc5000
walker <walker.shixy@gmail.com>
2014-04-17 15:52:55 +0800

    update pages

--- js/application.js
+++ js/application.js
@@ -71,33 +71,6 @@
       $('.bs-top').affix()
     }, 100)
 
-    // tooltip demo
-    $('.tooltip-demo').tooltip({
-      selector: "[data-toggle=tooltip]",
-      container: "body"
-    })
-
-    $('.tooltip-test').tooltip()
-    $('.popover-test').popover()
-
-    $('.bs-docs-navbar').tooltip({
-      selector: "a[data-toggle=tooltip]",
-      container: ".bs-docs-navbar .nav"
-    })
-
-    // popover demo
-    $("[data-toggle=popover]")
-      .popover()
-
-    // button state demo
-    $('#fat-btn')
-      .click(function () {
-        var btn = $(this)
-        btn.button('loading')
-        setTimeout(function () {
-          btn.button('reset')
-        }, 3000)
-      })
 })
 
 }(jQuery)
