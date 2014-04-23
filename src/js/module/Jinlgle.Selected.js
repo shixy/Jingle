17c7c944dfc46f9a9f974c8ae5d81a037a2eccbd
walker <walker.shixy@gmail.com>
2014-04-23 09:51:49 +0800

    缩短检测时间，仅仅是加入队列

--- src/js/module/Jinlgle.Selected.js
+++ src/js/module/Jinlgle.Selected.js
@@ -7,13 +7,12 @@ J.Selected = (function($){
     var SELECTOR = '[data-selected]',
         activeEl,timer;
     var init = function(){
-        //添加命名空间，防止冲突
         $(document).on('touchstart.selected',SELECTOR,function(){
             var $el = $(this);
             //在滑动的时候有闪烁，添加一个延时器,防止误操作
             timer = setTimeout(function(){
                 activeEl = $el.addClass($el.data('selected'));
-            },50);
+            },0);
 
         });
         $(document).on('touchmove.selected touchend.selected touchcancel.selected',function(){
