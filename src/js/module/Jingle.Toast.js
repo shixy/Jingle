3e7f3f0d3d1a02199de695b5b2ae7f20c6fbecf1
walker <walker.shixy@gmail.com>
2014-05-11 15:31:35 +0800

    添加一个常用的顶部显示消息的api

--- src/js/module/Jingle.Toast.js
+++ src/js/module/Jingle.Toast.js
@@ -2,15 +2,15 @@
  *  消息组件
  */
 J.Toast = (function($){
-    var TOAST_DURATION = 3000;
-    //定义模板
-    var TEMPLATE = {
-        toast : '<a href="#">{value}</a>',
-        success : '<a href="#"><i class="icon checkmark-circle"></i>{value}</a>',
-        error : '<a href="#"><i class="icon cancel-circle"></i>{value}</a></div>',
-        info : '<a href="#"><i class="icon info-2"></i>{value}</a>'
-    }
-    var toast_type = 'toast',_toast,timer;
+    var toast_type = 'toast',_toast,timer,
+        //定义模板
+        TEMPLATE = {
+            toast : '<a href="#">{value}</a>',
+            success : '<a href="#"><i class="icon checkmark-circle"></i>{value}</a>',
+            error : '<a href="#"><i class="icon cancel-circle"></i>{value}</a></div>',
+            info : '<a href="#"><i class="icon info-2"></i>{value}</a>'
+        }
+
     var _init = function(){
         //全局只有一个实例
         $('body').append('<div id="jingle_toast"></div>');
@@ -29,17 +29,18 @@ J.Toast = (function($){
     }
     /**
      * 显示消息提示
-     * @param type 类型  toast|success|error|info
+     * @param type 类型  toast|success|error|info  空格 + class name 可以实现自定义样式
      * @param text 文字内容
      * @param duration 持续时间 为0则不自动关闭,默认为3000ms
      */
     var show = function(type,text,duration){
         if(timer) clearTimeout(timer);
-        toast_type = type;
-        _toast.attr('class',type).html(TEMPLATE[type].replace('{value}',text)).show();
+        var classname = type.split(/\s/);
+        toast_type = classname[0];
+        _toast.attr('class',type).html(TEMPLATE[toast_type].replace('{value}',text)).show();
         J.anim(_toast,'scaleIn');
         if(duration !== 0){//为0 不自动关闭
-            timer = setTimeout(hide,duration || TOAST_DURATION);
+            timer = setTimeout(hide,duration || J.settings.toastDuration);
         }
     }
     var _subscribeCloseTag = function(){
